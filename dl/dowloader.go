ce4633db98afc5d43b969e135d267e01c0574e50
nullptrX <nullptrxxx@gmail.com>
2021-08-14 01:28:53 +0800

    断点续传

--- dl/dowloader.go
+++ dl/dowloader.go
@@ -115,54 +115,56 @@ func (d *Downloader) Start(concurrency int) error {
 
 func (d *Downloader) download(segIndex int) error {
 	tsFilename := tsFilename(segIndex)
-	tsUrl := d.tsURL(segIndex)
-	b, e := tool.Get(tsUrl)
-	if e != nil {
-		return fmt.Errorf("request %s, %s", tsUrl, e.Error())
-	}
-	//noinspection GoUnhandledErrorResult
-	defer b.Close()
 	fPath := filepath.Join(d.tsFolder, tsFilename)
-	fTemp := fPath + tsTempFileSuffix
-	f, err := os.Create(fTemp)
-	if err != nil {
-		return fmt.Errorf("create file: %s, %s", tsFilename, err.Error())
-	}
-	bytes, err := ioutil.ReadAll(b)
-	if err != nil {
-		return fmt.Errorf("read bytes: %s, %s", tsUrl, err.Error())
-	}
-	sf := d.result.M3u8.Segments[segIndex]
-	if sf == nil {
-		return fmt.Errorf("invalid segment index: %d", segIndex)
-	}
-	key, ok := d.result.Keys[sf.KeyIndex]
-	if ok && key != "" {
-		bytes, err = tool.AES128Decrypt(bytes, []byte(key),
-			[]byte(d.result.M3u8.Keys[sf.KeyIndex].IV))
+	if !tool.Exists(fPath) {
+		tsUrl := d.tsURL(segIndex)
+		b, e := tool.Get(tsUrl)
+		if e != nil {
+			return fmt.Errorf("request %s, %s", tsUrl, e.Error())
+		}
+		//noinspection GoUnhandledErrorResult
+		defer b.Close()
+		fTemp := fPath + tsTempFileSuffix
+		f, err := os.Create(fTemp)
 		if err != nil {
-			return fmt.Errorf("decryt: %s, %s", tsUrl, err.Error())
+			return fmt.Errorf("create file: %s, %s", tsFilename, err.Error())
 		}
-	}
-	// https://en.wikipedia.org/wiki/MPEG_transport_stream
-	// Some TS files do not start with SyncByte 0x47, they can not be played after merging,
-	// Need to remove the bytes before the SyncByte 0x47(71).
-	syncByte := uint8(71) //0x47
-	bLen := len(bytes)
-	for j := 0; j < bLen; j++ {
-		if bytes[j] == syncByte {
-			bytes = bytes[j:]
-			break
+		bytes, err := ioutil.ReadAll(b)
+		if err != nil {
+			return fmt.Errorf("read bytes: %s, %s", tsUrl, err.Error())
 		}
-	}
-	w := bufio.NewWriter(f)
-	if _, err := w.Write(bytes); err != nil {
-		return fmt.Errorf("write to %s: %s", fTemp, err.Error())
-	}
-	// Release file resource to rename file
-	_ = f.Close()
-	if err = os.Rename(fTemp, fPath); err != nil {
-		return err
+		sf := d.result.M3u8.Segments[segIndex]
+		if sf == nil {
+			return fmt.Errorf("invalid segment index: %d", segIndex)
+		}
+		key, ok := d.result.Keys[sf.KeyIndex]
+		if ok && key != "" {
+			bytes, err = tool.AES128Decrypt(bytes, []byte(key),
+				[]byte(d.result.M3u8.Keys[sf.KeyIndex].IV))
+			if err != nil {
+				return fmt.Errorf("decryt: %s, %s", tsUrl, err.Error())
+			}
+		}
+		// https://en.wikipedia.org/wiki/MPEG_transport_stream
+		// Some TS files do not start with SyncByte 0x47, they can not be played after merging,
+		// Need to remove the bytes before the SyncByte 0x47(71).
+		syncByte := uint8(71) //0x47
+		bLen := len(bytes)
+		for j := 0; j < bLen; j++ {
+			if bytes[j] == syncByte {
+				bytes = bytes[j:]
+				break
+			}
+		}
+		w := bufio.NewWriter(f)
+		if _, err := w.Write(bytes); err != nil {
+			return fmt.Errorf("write to %s: %s", fTemp, err.Error())
+		}
+		// Release file resource to rename file
+		_ = f.Close()
+		if err = os.Rename(fTemp, fPath); err != nil {
+			return err
+		}
 	}
 	// Maybe it will be safer in this way...
 	atomic.AddInt32(&d.finish, 1)
